8ef5bf2fde07ea23286a0a53a4444e4cbc27aec0
arjun98k <arjunkandekar14@gmail.com>
2024-03-29 23:14:43 +0530

    update to arrays

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-// store input numbers
-const num1 = parseInt(prompt('Enter the first number '));
-const num2 = parseInt(prompt('Enter the second number '));
 
-//add two numbers
-const sum = num1 + num2;
 
-// display the sum
-console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+const Num_id = 98
+console.log(Num_id)
