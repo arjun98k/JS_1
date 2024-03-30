3c28cce71af45ae6950c919b9abf68feee37815c
arjun98k <arjunkandekar14@gmail.com>
2024-03-30 12:55:49 +0530

    update github file

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const Num_id = 98
 num2 = "arjun kandekar"
 let num3 = 777
 let hari;
+/* prefer not to use var
+because of issue in block scope and functional scope
+*/
 
 
 console.table([Num_id,num2,num3,hari])
