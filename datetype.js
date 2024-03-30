a3606b522700b9c2e6ca7b3eb4b0c2137e385783
arjun98k <arjunkandekar14@gmail.com>
2024-03-31 00:15:25 +0530

    update github file

--- datetype.js
+++ datetype.js
@@ -2,3 +2,17 @@
 // alert (3+3) //we are using nodejs, not browser
 
 console.log("i love shri")
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined)
+console.log(typeof null)
