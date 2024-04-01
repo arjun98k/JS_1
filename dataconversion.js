1b094e1646753db7810a5ed2b64bd0f51c4b1a09
arjun98k <arjunkandekar14@gmail.com>
2024-04-02 00:41:30 +0530

    update github file

--- dataconversion.js
+++ dataconversion.js
@@ -2,3 +2,10 @@
  var kaju = "15"
 console.log(typeof  adi )
 console.log(typeof kaju)
+
+let valueinnumber = Number(kaju)
+console.log(typeof valueinnumber)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
