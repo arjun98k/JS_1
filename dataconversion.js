015cef67e1e043d86015c8f2cd6e384e838cadc6
arjun98k <arjunkandekar14@gmail.com>
2024-04-04 20:46:25 +0530

    update github file

--- dataconversion.js
+++ dataconversion.js
@@ -1,11 +1,18 @@
  var adi = "arjun"
  var kaju = "15"
-console.log(typeof  adi )
-console.log(typeof kaju)
+// console.log(typeof  adi )
+// console.log(typeof kaju)
 
 let valueinnumber = Number(kaju)
-console.log(typeof valueinnumber)
+// console.log(typeof valueinnumber)
 
 // "33" => 33
 // "33abc" => NaN
 //  true => 1; false => 0
+
+
+let RandomNumber = 98
+
+let StringNumber = String(RandomNumber)
+console.log(StringNumber)
+console.log(typeof StringNumber)
