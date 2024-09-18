8fa43ab3cccc8be192549046a088f7f7aa44bd50
arjun98k <arjunkandekar14@gmail.com>
2024-09-18 19:57:14 +0530

    hello java script we have to do

--- Akash.js
+++ Akash.js
@@ -1,1 +1,12 @@
-console.log('akash');
+var Account_name = "Aditya Jadhav";
+let Account_mail = "Aditya35@gmail.com";
+
+const roll_no = 12;
+
+ // prefer not to use var
+
+// because of issue in block scope and fcunctional scope 
+ 
+ /*
+ prefer most let  in modern javascript
+ */
